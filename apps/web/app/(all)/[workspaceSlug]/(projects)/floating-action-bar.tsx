71b8b6a73a2a7e4ebad3ff919e4395ff8c717d14
Akshita Goyal <36129505+gakshita@users.noreply.github.com>
2025-10-31 13:40:17 +0530

    [PAI-927] fix: issue canvas not opening outside pi-app (#4616)
    * fix: issue canvas not opening outside pi-app
    
    * fix: refactor

--- apps/web/app/(all)/[workspaceSlug]/(projects)/floating-action-bar.tsx
+++
@@ -1,8 +0,0 @@
-type Props = {
-  children: React.ReactNode;
-};
-export const FloatingActionsRoot = ({ children }: Props) => (
-  <div className="absolute bottom-4 right-4 z-[25] flex flex-col gap-4" id="floating-bot">
-    {children}
-  </div>
-);

e1d67cc68d2ffb562e4c8689a9da6e5b8d55af0a
Vipin Chaudhary <VipinChaudhary1809@gmail.com>
2025-11-19 12:44:35 +0530

    [WEB-5450] fix: enable unique ID flag for editor functionality in useEditorFlagging hook (#4788)

--- apps/web/ee/hooks/use-editor-flagging.ts
+++ apps/web/ee/hooks/use-editor-flagging.ts
@@ -28,8 +28,7 @@ export const useEditorFlagging = (props: TEditorFlaggingHookProps): TEditorFlagg
   const { isNestedPagesEnabled, isCommentsEnabled } = usePageStore(storeType || EPageStoreType.WORKSPACE);
   const isEditorAttachmentsEnabled = useFlag(workspaceSlug, "EDITOR_ATTACHMENTS");
   const isEditorCopyBlockLinkEnabled = useFlag(workspaceSlug, "EDITOR_COPY_BLOCK_LINK");
-  // const isEditorUniqueIdEnabled = useFlag(workspaceSlug, "EDITOR_UNIQUE_ID");
-  const isEditorUniqueIdEnabled = true;
+  const isEditorUniqueIdEnabled = useFlag(workspaceSlug, "EDITOR_UNIQUE_ID");
   const isEditorMathematicsEnabled = useFlag(workspaceSlug, "EDITOR_MATHEMATICS");
   const isExternalEmbedEnabled = useFlag(workspaceSlug, "EDITOR_EXTERNAL_EMBEDS");
   // check integrations

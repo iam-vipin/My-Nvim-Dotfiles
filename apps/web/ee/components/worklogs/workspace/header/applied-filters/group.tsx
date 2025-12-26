9444fbfc696e7b0d1db9bc855459959b9f6438fe
Vamsi Krishna <46787868+vamsikrishnamathala@users.noreply.github.com>
2025-12-26 13:37:43 +0530

    [WEB-5752] fix: work logs applied filters overflow #5229

--- apps/web/ee/components/worklogs/workspace/header/applied-filters/group.tsx
+++ apps/web/ee/components/worklogs/workspace/header/applied-filters/group.tsx
@@ -1,5 +1,6 @@
-import type { FC, ReactNode } from "react";
+import type { ReactNode } from "react";
 import { CloseIcon } from "@plane/propel/icons";
+import { IconButton } from "@plane/propel/icon-button";
 
 type TAppliedFilterGroup = {
   groupTitle: string;
@@ -13,13 +14,8 @@ export function AppliedFilterGroup(props: TAppliedFilterGroup) {
   return (
     <div className="relative flex items-center gap-2 p-1 px-2 rounded-sm border border-subtle-1">
       <div className="text-11 font-medium text-secondary capitalize">{groupTitle}</div>
-      <div className="flex items-center gap-2">{children}</div>
-      <div
-        className="rounded-sm flex-shrink-0 w-4 h-4 flex justify-center items-center cursor-pointer transition-all bg-layer-1 hover:bg-surface-1 text-secondary hover:text-primary"
-        onClick={onClear}
-      >
-        <CloseIcon className="w-3 h-3" />
-      </div>
+      <div className="flex items-center gap-2 flex-wrap">{children}</div>
+      <IconButton icon={CloseIcon} size="sm" onClick={onClear} variant={"ghost"} />
     </div>
   );
 }

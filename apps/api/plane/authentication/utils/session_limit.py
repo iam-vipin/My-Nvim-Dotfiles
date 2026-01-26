7e631abc32c326a85391ca994dccf219c44e951d
sriram veeraghanta <veeraghanta.sriram@gmail.com>
2026-01-27 00:41:23 +0530

    [SECUR-102] security: invalidate the sessions on password change #5560

--- apps/api/plane/authentication/utils/session_limit.py
+++ apps/api/plane/authentication/utils/session_limit.py
@@ -17,6 +17,23 @@ from django.utils import timezone
 from plane.db.models import Session
 
 
+def invalidate_all_user_sessions(user, exclude_session_key=None):
+    """
+    Invalidate all sessions for a user except the current one.
+
+    This should be called when a user changes their password to ensure
+    all other sessions are logged out for security.
+
+    Args:
+        user: The user object for whom to invalidate sessions
+        exclude_session_key: The session key to exclude from deletion (usually the current session)
+    """
+    sessions = Session.objects.filter(user_id=str(user.id))
+    if exclude_session_key:
+        sessions = sessions.exclude(session_key=exclude_session_key)
+    sessions.delete()
+
+
 def enforce_session_limit(user, current_session_key=None):
     """
     Enforce the maximum concurrent session limit for a user.
